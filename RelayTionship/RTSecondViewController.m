b883158c67ef7a25b8de641254f9f94402b5281e
Dru Kepple <drukepple@gmail.com>
2012-08-20 19:44:00 -0400

    Initial Commit

---
+++ RelayTionship/RTSecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  RTSecondViewController.m
+//  RelayTionship
+//
+//  Created by Dru Kepple on 8/20/12.
+//  Copyright (c) 2012 Dru Kepple. All rights reserved.
+//
+
+#import "RTSecondViewController.h"
+
+@interface RTSecondViewController ()
+
+@end
+
+@implementation RTSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+	    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	} else {
+	    return YES;
+	}
+}
+
+@end
---
+++ RelayTionship/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RelayTionship
+//
+//  Created by Dru Kepple on 8/20/12.
+//  Copyright (c) 2012 Dru Kepple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+	}
+}
